ba949a65d3a387249a093ad8b3729946b8bb9c68
Carlos Daniel Albino Martínez <albinomartinezcarlosdaniel@gmail.com>
2021-03-11 17:31:33 -0600

    Agregando servicio de Mailchimp2.1

--- src/app/services/suscripcion/suscripcion.service.ts
+++ src/app/services/suscripcion/suscripcion.service.ts
@@ -19,7 +19,7 @@ export class SuscripcionService {
     .set('EMAIL', data)
     .set('group[80463][2]', 'true')
     .set('b_524d6e87dd4ea8d25621ea01f&id_042db4d02b', '');
-    /* const mailChimpUrl = `${this.baseUrl}&${params.toString()}`;
-    return this.http.jsonp(mailChimpUrl, 'c'); */
+    const mailChimpUrl = `${this.baseUrl}&${params.toString()}`;
+    return this.http.jsonp(mailChimpUrl, 'c');
   }
 }
